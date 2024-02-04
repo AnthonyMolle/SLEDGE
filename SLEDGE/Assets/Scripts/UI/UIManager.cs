6d05f91258b94209d67a4795ef214d496ea31b80
Jonah Ryan <jopryan@ucsc.edu>
2024-02-03 17:54:42 -0800

    Add setting transition
    Now we can transition between two UI Canvases! The setting canvas is set up and next needs to be filled

---
+++ SLEDGE/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public float fadeDuration = 1;
+    public AnimationCurve animCurve;
+
+    public void TransitionTo(CanvasGroup a)
+    {
+        CanvasGroup currentCanvas = null;
+        CanvasGroup[] b = FindObjectsOfType<CanvasGroup>();
+        for (int i = 0; i < b.Length; i++)
+        {
+            if (b[i].alpha == 1)
+            {
+                currentCanvas = b[i];
+            }
+        }
+
+        StartCoroutine(Fade(a,0,1));
+        if (currentCanvas != null)
+        {
+            StartCoroutine(Fade(currentCanvas, 1, 0));
+        }
+    }
+
+    IEnumerator Fade(CanvasGroup a, float startAlpha, float endAlpha)
+    {
+        a.gameObject.SetActive(true);
+        a.interactable = false;
+
+        float timeElapsed = 0;
+
+        while (timeElapsed < fadeDuration)
+        {
+            float t = timeElapsed / fadeDuration;
+
+            t = animCurve.Evaluate(t);
+
+            a.alpha = Mathf.Lerp(startAlpha, endAlpha, t);
+
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        a.alpha = endAlpha;
+        if (endAlpha > 0.5) { a.interactable = true;}  
+        if (endAlpha < 0.5) { a.gameObject.SetActive(false); }
+    }
+}
