342092388e88ce5c8b6a147345a40492511d57e0
Jonah Ryan <jopryan@ucsc.edu>
2024-02-21 00:30:34 -0800

    Flying Enemy Implemented
    - Killed my darlings, no more A* now using a system that records that players path
    - If the enemies can see you they go straight for you, if they cant they follow your path to reach you
    - Idle then permanently aggro at an adjustable alert distance.
    - No attacking implemented
    
    To set up
    - Add player tracker script to player
    - Add flying enemy prefab into scene
    - Give enemy the enemy layer or ignoreRaycast layer

---
+++ SLEDGE/Assets/Scripts/Enemies/FlyingEnemy.cs
@@ -0,0 +1,152 @@
+using UnityEngine;
+
+// Jonah Ryan
+
+public class FlyingEnemy : MonoBehaviour
+{
+    [Header("Movement Properties")]
+    public float movementSpeed = 5;
+    
+
+    [Header("Vision Properties")]
+    public float detectionRadius = 5;
+    public LayerMask visionBlocking;
+
+    [Header("Debug")]
+    public Color huntingColor;
+    public Color pathingColor;
+
+
+    Rigidbody rb;
+    Vector3 targetPos;
+    Vector3 currentWaypoint;
+    Transform player;
+    int currentIndexOnPath = -1;
+    RaycastHit hit;
+
+    public enum EnemyState
+    {
+        IDLE,
+        DIRECTHUNT,
+        ONPATH
+    }
+
+    EnemyState enemyState = EnemyState.IDLE;
+
+    // Setup our dependencies
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    // Check if a given transform at endPoint is visible from a startPoint
+    // Visible means a raycast can be drawn between the two points without hitting a collider
+    private bool transformInView(Vector3 startPoint, Vector3 endPoint, Transform targetTransform, float distanceOfVision)
+    {
+        Vector3 EnemytoPlayer = endPoint - startPoint;
+
+        if (Physics.Raycast(startPoint, EnemytoPlayer, out hit, distanceOfVision, visionBlocking))
+        {
+            if (hit.transform == targetTransform)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Process our current state
+    private void FixedUpdate()
+    {
+
+        rb.velocity = Vector3.zero;
+
+        switch (enemyState)
+        {
+            /* 
+             * Idle state:
+             * - Waits for player to be in range
+             * - Then starts hunting player
+            */
+            case EnemyState.IDLE:
+
+                // Aggro when player 10 units away
+                if (transformInView(transform.position, player.position, player, detectionRadius))
+                {
+                    enemyState = EnemyState.DIRECTHUNT;
+                }
+                break;
+
+            /*
+             * Hunt State:
+             * - When player is visible go straight towards them
+             * - Otherwise, switch to pathing mode
+             */
+            case EnemyState.DIRECTHUNT:
+
+                GetComponent<Renderer>().material.color = huntingColor;
+
+                // Move towards player
+                targetPos = Vector3.MoveTowards(transform.position, player.position, movementSpeed * Time.deltaTime);
+                rb.MovePosition(targetPos);
+
+                // If player not in line of sight, pathfind to player
+                if (!transformInView(transform.position, player.position, player, Mathf.Infinity))
+                {
+                    enemyState = EnemyState.ONPATH;
+                }
+
+                break;
+
+            /*
+             * Path state:
+             *  - When player not visible, follow known path
+             *  - When player visible, go back to hunt state
+             */
+            case EnemyState.ONPATH:
+
+                GetComponent<Renderer>().material.color = pathingColor;
+
+                // Initialize path following with closest node on path
+                if (currentIndexOnPath == -1)
+                {
+                    currentIndexOnPath = PlayerTracker.getNearestOnPath(transform.position);
+                    currentWaypoint = PlayerTracker.getIndexOnPath(currentIndexOnPath);
+                }
+
+                // If you can see player, leave pathing, enter hunt state
+                if (transformInView(transform.position, player.position, player, Mathf.Infinity))
+                {
+                    enemyState = EnemyState.DIRECTHUNT;
+                    currentIndexOnPath = -1;
+                    break;
+                }
+
+                // If reached current path target, go forward on path
+                if(Vector3.Distance(transform.position, currentWaypoint) < 1)
+                {
+                    currentIndexOnPath += 1;
+
+                    // If reached end of path, restart path following with new path information
+                    if(PlayerTracker.getSize() <= currentIndexOnPath)
+                    {
+                        currentIndexOnPath = PlayerTracker.getNearestOnPath(transform.position);
+                        currentWaypoint = PlayerTracker.getIndexOnPath(currentIndexOnPath);
+                        break;
+                    }
+
+                    // Update target to next on path
+                    currentWaypoint = PlayerTracker.getIndexOnPath(currentIndexOnPath);
+                }
+
+                // Move towards our current path target
+                targetPos = Vector3.MoveTowards(transform.position, currentWaypoint, movementSpeed * Time.deltaTime);
+                rb.MovePosition(targetPos);
+
+                break;
+
+        }
+    }
+}
