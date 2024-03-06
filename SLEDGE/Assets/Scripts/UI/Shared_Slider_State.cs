70b00139cce1615d373bd4fbe2681e2f97683a72
Jonah Ryan <jopryan@ucsc.edu>
2024-03-06 01:32:31 -0800

    Add Sliders text adjustments
    Can adjust sliders values with the text input field and they sync together
    - Added 3 new language items

---
+++ SLEDGE/Assets/Scripts/UI/Shared_Slider_State.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Shared_Slider_State : MonoBehaviour
+{
+    public float value;
+    public float min;
+    public float max;
+
+    public TMP_InputField textBox;
+    public Slider slider;
+
+    public UnityEvent onValueChange;
+
+    private void Awake()
+    {
+        textBox.onSubmit.AddListener(delegate { TextUpdated(); });
+        slider.onValueChanged.AddListener(delegate { SliderUpdate(); });
+    }
+    public void TextUpdated()
+    {
+        if (float.TryParse(textBox.text, out float result))
+        {
+            UpdateValue(result);
+        }  
+    }
+
+    public void SliderUpdate()
+    {
+        UpdateValue(slider.value);
+    }
+
+    public void UpdateValue(float value)
+    {
+        onValueChange.Invoke();
+        this.value = Mathf.Clamp(value, min, max);
+        textBox.text = Mathf.Round(value).ToString();
+        slider.value = value;
+    }
+}
