c221232030b05241e5cfb22e27d4c6fec3e55412
CaptainJoystick <AdiTyagi285@gmail.com>
2024-04-07 13:55:37 -0700

    headbob

--- SLEDGE/Assets/Scripts/Player/PlayerController.cs
+++ SLEDGE/Assets/Scripts/Player/PlayerController.cs
@@ -833,4 +833,9 @@ public class PlayerController : MonoBehaviour
 
         currentHealth = maxHealth;
     }
+
+    public bool CheckMoving()
+    {
+        return movementInputVector.magnitude != 0;
+    }
 }
