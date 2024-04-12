9764c698a99fee9b74964d6987a3bc66a254eeb2
domothebomo <dfanaris@gmail.com>
2024-04-11 20:29:32 -0700

    Added very basic collectible prototype (still needs to be plugged in to a manager to actually count towards anything)

---
+++ SLEDGE/Assets/Scripts/Level/Collectible.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collectible found!");
+            // Add collectible to score manager
+            Destroy(gameObject);
+        }
+    }
+}
