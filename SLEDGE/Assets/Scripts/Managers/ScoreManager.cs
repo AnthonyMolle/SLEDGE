d03991ab513bbb44fa53b69d17fa039c13c03a59
Jonah Ryan <jonahrobot@gmail.com>
2024-04-16 12:44:31 -0700

    Add Score Manager
    Singleton that can be added per stage.
    
    To use in any class without direct reference to ScoreManager
    
    ScoreManager.Instance.TimesLanded += 0;
    or
    a = ScoreManager.Instance.TimesLanded;

--- SLEDGE/Assets/Scripts/Managers/ScoreManager.cs
+++ SLEDGE/Assets/Scripts/Managers/ScoreManager.cs
@@ -1,44 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Acts as a data aggregate for other objects to use
+// Victory screen and PlayerSaveData get data from this
+// Other script write to this data
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager Instance;
+
+    // Data links
+    public Speed_Run_Timer timer;
+
+    private void Awake()
+    {
+        if(Instance != this && Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
 
     #region Collectibles
-    public int MaxCollectibles = 1;
-    int Collectibles = 0;
+    public int MaxCollectibles { get; set; }
+    public int CollectiblesFound { get; set; }
     #endregion
 
-    //public int MaxEnemies = 0;
-    int enemiesKilled = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    #region Speedrunning
+    public float CurrentTime {
+        // Avoids updating timer every frame in two places
+        get
+        {
+            return timer.GetTimeFloat();
+        }
     }
+    #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    #region Combat
+    public int EnemiesKilled { get; set; }
+    public int StyleKills { get; set; }
+    public float DamageTaken { get; set; }
+    #endregion
 
-    public int GetCollectibles()
-    {
-        return Collectibles;
-    }
-
-    public int GetMaxCollectibles()
-    {
-        return MaxCollectibles;
-    }
+    #region Movement
+    public int TimesLanded { get; set; }
+    #endregion
 
     public void PickUpCollectible(GameObject collectible)
     {
         // Possibly track specific collectibles later?
-        Collectibles++;
-        Debug.Log("Collectibles found: " + Collectibles);
+        CollectiblesFound++;
+        Debug.Log("Collectibles found: " + CollectiblesFound);
     }
 }
