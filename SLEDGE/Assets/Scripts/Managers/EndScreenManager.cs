fff9df5716dfc832aa4274ede58239a3b4ea1f06
Jonah Ryan <jonahrobot@gmail.com>
2024-04-23 22:56:36 -0700

    Add end level screen animations
    - Background comes into scene
    - Scores pop in
    - Final grade shows
    - Grades are hard coded rn
    - Game does not trigger this scene

---
+++ SLEDGE/Assets/Scripts/Managers/EndScreenManager.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndScreenManager : MonoBehaviour
+{
+    public EndScreenScore Time;
+    public EndScreenScore Kills;
+    public EndScreenScore StyleKills;
+
+    public TextMeshProUGUI finalGradeText;
+    public GameObject dividerTwo;
+
+    public bool triggerDropIn;
+    private bool triggerReveal;
+
+    public float secBetweenReveals = 0.5f;
+    public float secBetweenScoreAndGrade = 0.5f;
+    public float secMultiplier = 0.65f;
+
+    string stageTime, kills, styleKills;
+    Grade timeGrade, killGrade, styleGrade, finalGrade;
+    private RectTransform RectTransform;
+
+    public enum Grade
+    {
+        F, // 0
+        D, // 1
+        C, // 2
+        B, // 3
+        A  // 4
+    }
+    private void Start()
+    {
+        finalGradeText.text = "";
+        dividerTwo.SetActive(false);
+        RectTransform = GetComponent<RectTransform>();
+        RectTransform.sizeDelta = new Vector2(640.3f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggerDropIn)
+        {
+            Vector2 sizeDelta = RectTransform.sizeDelta;
+
+            sizeDelta = new Vector2(640.3f, Mathf.Lerp(sizeDelta.y, 1500, 2 * UnityEngine.Time.deltaTime));
+
+            if(sizeDelta.y >= 1420)
+            {
+                triggerReveal = true;
+                triggerDropIn = false;
+            }
+
+            RectTransform.sizeDelta = sizeDelta;
+        }
+
+        if (triggerReveal)
+        {
+            
+
+            triggerReveal = false;
+
+            // Get Scores
+            stageTime = ScoreManager.Instance.GetCurrentTime().ToString();
+            kills = "kills: " + ScoreManager.Instance.GetEnemiesKilled().ToString();
+            styleKills = "styles: " + ScoreManager.Instance.GetStyleKills().ToString();
+
+            // Calculate Grades
+            timeGrade = Grade.F;
+            killGrade = Grade.F;
+            styleGrade = Grade.C;
+            finalGrade = Grade.F;
+
+            // Match expectations of time
+            secBetweenReveals += secBetweenScoreAndGrade;
+
+            StartCoroutine(reveal());
+        }
+    }
+
+    IEnumerator reveal()
+    {
+
+        // Reveal Time
+        Time.triggerReveal(secBetweenScoreAndGrade, stageTime, GradeToString(timeGrade));
+
+            yield return new WaitForSeconds(secBetweenReveals);
+
+        // Reveal Kills
+        Kills.triggerReveal(secBetweenScoreAndGrade * secMultiplier, kills, GradeToString(killGrade));
+
+            yield return new WaitForSeconds(secBetweenReveals * secMultiplier);
+
+        // Reveal Style Kills
+        StyleKills.triggerReveal(secBetweenScoreAndGrade * secMultiplier * secMultiplier, styleKills, GradeToString(styleGrade));
+
+            yield return new WaitForSeconds(secBetweenReveals * secMultiplier * secMultiplier);
+
+        // Show final grade
+        dividerTwo.SetActive(true);
+        finalGradeText.text = GradeToString(finalGrade);
+    }
+
+    private string GradeToString(Grade g)
+    {
+        switch (g)
+        {
+            case Grade.A:
+                return "A";
+            case Grade.B:
+                return "B";
+            case Grade.C:
+                return "C";
+            case Grade.D:
+                return "D";
+            case Grade.F:
+                return "F";
+            default:
+                return "NAN";
+        }
+    }
+
+}
+
+
