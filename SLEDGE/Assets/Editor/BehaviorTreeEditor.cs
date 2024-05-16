c30ec438b0d02de98fab11fb02ee21b3db9be3d6
Jonah Ryan <jonahrobot@gmail.com>
2024-05-15 22:31:23 -0700

    Added some temp UI stuff!

---
+++ SLEDGE/Assets/Editor/BehaviorTreeEditor.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BehaviorTreeEditor : EditorWindow
+{
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    [MenuItem("BehaviorTreeEditor/Editor ...")]
+    public static void OpenWindow()
+    {
+        BehaviorTreeEditor wnd = GetWindow<BehaviorTreeEditor>();
+        wnd.titleContent = new GUIContent("BehaviorTreeEditor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/BehaviorTreeEditor.uxml");
+        visualTree.CloneTree(root);
+
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/BehaviorTreeEditor.uss");
+        root.styleSheets.Add(styleSheet);
+    }
+}
