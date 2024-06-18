766200998808057f2920054997f054bb62041d78
Jonah Ryan <jonahrobot@gmail.com>
2024-06-18 00:23:54 -0700

    Add pathing to enemies

--- SLEDGE/Assets/Behavior_Tree_Package/Action Nodes/MoveTowards_Action.cs
+++ SLEDGE/Assets/Behavior_Tree_Package/Action Nodes/MoveTowards_Action.cs
@@ -1,23 +1,27 @@
-using System.Reflection;
-using UnityEditor;
 using UnityEngine;
 
 public class MoveTowards_Action : ActionNode
 {
-    Transform currentTransform;
-    Rigidbody currentRigidbody;
+    // Dependencies
+    Transform rootTransform;
+    Rigidbody rootRigidbody;
+    GameObject currentTarget;
 
-    GameObject target;
-
+    // Public Parameters
     public Blackboard.ObjectOptions objectTarget;
     public float speed;
 
+    // Internal State tracking
+    int currentPathIndex = -1;
+    Vector3 currentPathPoint;
+    bool pathing = false;
+
     protected override void OnStart()
     {
-        currentTransform = blackboard.getCurrentRunner().transform;
-        currentRigidbody = blackboard.getCurrentRunner().GetComponent<Rigidbody>();
+        rootTransform = blackboard.getCurrentRunner().transform;
+        rootRigidbody = blackboard.getCurrentRunner().GetComponent<Rigidbody>();
 
-        target = blackboard.getObject(objectTarget);
+        currentTarget = blackboard.getObject(objectTarget);
     }
 
     protected override void OnStop()
@@ -26,15 +30,68 @@ public class MoveTowards_Action : ActionNode
 
     protected override State OnUpdate()
     {
-        // Do stuff
-        Vector3 targetDirection = (target.transform.position - currentTransform.position).normalized;
-        currentRigidbody.velocity = targetDirection * speed;
+        if (isTargetInView())
+        {
+            MoveTowardsTarget(currentTarget.transform.position);
+
+            if(pathing) pathing = false;
+        }
+        else
+        {
+            if (pathing == false)
+            {
+                currentPathIndex = PlayerTracker.getPathIndex(rootTransform.position);
+                currentPathPoint = PlayerTracker.getPointFromIndex(currentPathIndex);
+                pathing = true;
+            }
+
+            bool reachedPoint = Vector3.Distance(rootTransform.position, currentPathPoint) < 1;
+
+            if (reachedPoint)
+            {
+                currentPathIndex += 1;
+
+                bool pathFinished = PlayerTracker.getSize() <= currentPathIndex;
+
+                if (pathFinished)
+                {
+                    currentPathIndex = PlayerTracker.getPathIndex(rootTransform.position);
+                }
+
+                currentPathPoint = PlayerTracker.getPointFromIndex(currentPathIndex);
+            }
+
+            MoveTowardsTarget(currentPathPoint);
+        }
 
         if (child != null)
         {
             child.state = State.Running;
             child.Update();
         }
+
         return State.Running;
+    }
+
+    private void MoveTowardsTarget(Vector3 currentTarget)
+    {
+        Vector3 targetDirection = (currentTarget - rootTransform.position).normalized;
+        rootRigidbody.velocity = targetDirection * speed;
+    }
+
+    private bool isTargetInView()
+    {
+        Vector3 currentPosToTarget = currentTarget.transform.position - rootTransform.position;
+        RaycastHit hit;
+
+        // Everything blocks this infinite raycast
+        if (Physics.Raycast(rootTransform.position, currentPosToTarget, out hit, Mathf.Infinity, ~0))
+        {
+            if (hit.transform == currentTarget.transform)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
