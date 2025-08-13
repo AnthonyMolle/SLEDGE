4da862ecd1d8b0f3b84176ece198bce906715553
Evan Pompa <55860113+PompaEvanB@users.noreply.github.com>
2025-08-13 12:28:25 -0700

    updated lvl1

---
+++ SLEDGE/Assets/SLEDGE/GameplayElements/LevelElements/MovingPlatform/RotatePlatform.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatform : MonoBehaviour
+{
+    /*
+    [serializedField] private float speed;
+    [serializedField] private float X;
+    [serializedField] private float Y;
+    [serializedField] private float Z;
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
